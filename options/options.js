38d4868e3864aa6793092363ab4622f34a38b98f
Crowdbotics <team@crowdbotics.com>
2023-08-27 02:55:20 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "rough-sound-43094",
+  url: "https://rough-sound-43094.botics.co",
+  api: "https://rough-sound-43094.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
